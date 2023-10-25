5f3958f34ef231cabb0113aecc9e49c1389bff7d
y0123456789 <52168408+y0123456789@users.noreply.github.com>
2023-10-25 21:18:29 +0800

    Update ipsw1.py

--- ipsw1.py
+++ ipsw1.py
@@ -2,9 +2,14 @@ import requests
 import json
 import datetime
 
+
 def format_date(date_str):
+    # 将日期字符串解析为datetime对象
     date = datetime.datetime.fromisoformat(date_str)
+
+    # 将datetime对象转换为年月日格式字符串
     return date.strftime("%Y.%m.%d")
+
 
 devices = {
     "device1": "iPhone",
@@ -17,19 +22,10 @@ for device_key, device_name in devices.items():
     response = requests.get(device_url)
     devices_data = response.json()
 
-    firmware_data = {}  # 用于存储相同identifier的固件数据
+    all_data = {}
 
     for device in devices_data:
         identifier = device.get("identifier")
-
-        if identifier not in firmware_data:
-            firmware_data[identifier] = {
-                "id": None,
-                "name": None,
-                "identifier": None,
-                "release_date": None,
-                "firmwares": []
-            }
 
         url1 = f"https://betahub.cn/api/apple/firmwares/{identifier}?type=1"
         url2 = f"https://betahub.cn/api/apple/firmwares/{identifier}?type=2"
@@ -37,32 +33,58 @@ for device_key, device_name in devices.items():
         response1 = requests.get(url1)
         data1 = response1.json()
 
-        # 合并固件数据
-        firmware_data[identifier]["firmwares"].extend(data1.get("firmwares", []))
+        filtered_data1 = {
+            "id": data1.get("id"),
+            "name": data1.get("name"),
+            "identifier": data1.get("identifier"),
+            "release_date": data1.get("release_date"),
+            "firmwares": []
+        }
+
+        for firmware1 in data1.get("firmwares", []):
+            filtered_firmware1 = {
+                "id": firmware1.get("id"),
+                "version": "iOS " + firmware1.get("version") if device_name == "iPhone" else
+                "iPadOS " + firmware1.get("version") if device_name == "iPad" else
+                "MacOS " + firmware1.get("version") if device_name == "Mac" else firmware1.get("version"),
+                "build_id": firmware1.get("build_id"),
+                "size": f"{round(firmware1.get('size') / 1073741824, 2):.2f}GB",
+                "url": firmware1.get("url"),
+                "created_at": firmware1.get("created_at"),
+                "type": firmware1.get("type"),
+                "signing": firmware1.get("signing")
+            }
+            filtered_data1["firmwares"].append(filtered_firmware1)
 
         if url2:
             response2 = requests.get(url2)
             data2 = response2.json()
 
-            # 同样，合并固件数据
-            firmware_data[identifier]["firmwares"].extend(data2.get("firmwares", []))
+            filtered_data2 = {
+                "id": data2.get("id"),
+                "name": data2.get("name"),
+                "identifier": data2.get("identifier"),
+                "release_date": data2.get("release_date"),
+                "firmwares": []
+            }
 
-    # 更新其他属性，如id、name和release_date
-    for identifier, data in firmware_data.items():
-        data["id"] = data1.get("id")
-        data["name"] = data1.get("name")
-        data["identifier"] = data1.get("identifier")
-        data["release_date"] = format_date(data1.get("release_date"))
+            for firmware2 in data2.get("firmwares", []):
+                filtered_firmware2 = {
+                    "id": firmware2.get("id"),
+                    "version": firmware2.get("version"),
+                    "build_id": firmware2.get("build_id"),
+                    "size": f"{round(firmware2.get('size') / 1073741824, 2):.2f}GB",
+                    "url": firmware2.get("url"),
+                    "created_at": firmware2.get("created_at"),
+                    "type": firmware2.get("type"),
+                    "signing": firmware2.get("signing")
+                }
+                filtered_data2["firmwares"].append(filtered_firmware2)
 
-    # 将每个固件数据中的created_at转换为年月日格式
-    for identifier, data in firmware_data.items():
-        for firmware in data["firmwares"]:
-            firmware["created_at"] = format_date(firmware["created_at"])
+            # 合并firmwares
+            filtered_data1["firmwares"].extend(filtered_data2["firmwares"])
 
-    # 输出为JSON格式并保存为以identifier命名的文件
-    for identifier, data in firmware_data.items():
-        output_json = json.dumps(data, ensure_ascii=False)
-        filename = f"{identifier}.json"
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(output_json)
-        print(f"已保存JSON文件: {filename}")
+            # 保存到JSON文件
+            with open(f"{identifier}.json", "w", encoding="utf-8") as json_file:
+                json.dump(filtered_data1, json_file, ensure_ascii=False, indent=4)
+
